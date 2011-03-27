482e4ab15f7b4454a44e6445bce36486abb3f88d
webnull <webnull.www@gmail.com>
2011-03-27 21:52:52 +0200

    First version

---
+++ wtop.py
@@ -0,0 +1,89 @@
+#!/usr/bin/python
+import sys
+import curses
+import getopt
+import time
+import os
+import wicd
+from wicd import dbusmanager
+from dbus import DBusException
+
+# connection to DBUS interface
+try:
+    dbusmanager.connect_to_dbus()
+except DBusException:
+        print "Cannot connect to WICD daemon, please be sure daemon is started before using wconfig. You can start daemon with /etc/init.d/wicd start, or /etc/rc.d/wicd start, or wicd from root account."
+        sys.exit()
+
+bus = dbusmanager.get_bus()
+dbus_ifaces = dbusmanager.get_dbus_ifaces()
+daemon = dbus_ifaces['daemon']
+wireless = dbus_ifaces['wireless']
+[state, info] = daemon.GetConnectionStatus()
+myscreen = curses.initscr()
+
+myscreen.clear()
+myscreen.border(0)
+myscreen.addstr(1, 1, "Scanning...")
+myscreen.refresh()
+
+def get_prop(net_id, prop):
+        """ Get attribute of wireless network """
+        return wireless.GetWirelessProperty(net_id, prop)
+
+def fix_strength(val, default):
+        """ Assigns given strength to a default value if needed. """
+        return val and int(val) or default
+
+# HISTORY
+networks = {}
+
+def main():
+    global myscreen, networks
+    myscreen.border(0)
+    i=0
+
+    [state, info] = daemon.GetConnectionStatus()
+    wireless.Scan(True)
+
+    num_networks = wireless.GetNumberOfNetworks()
+    myscreen.clear()
+    myscreen.border(0)
+
+    if num_networks > 0:
+        myscreen.addstr(1, 1, 'CURRENT SCAN RESULTS:')
+        i=1
+        for x in range(0, num_networks):
+            i=i+1
+            essid = str(get_prop(x, "essid"))
+            strength = str(fix_strength(get_prop(x, "quality"), -1))
+            encryption = str(get_prop(x, "encryption_method"))
+            channel = get_prop(x, 'channel')
+            mac = str(get_prop(x, "bssid"))
+
+            myscreen.addstr(i, 1, strength+"% ("+encryption+") "+mac+" "+essid+" channel "+channel)
+
+            try:
+                networks[essid]
+            except KeyError:
+                networks[essid] = {}
+                networks[essid]['signal'] = strength
+                networks[essid]['encryption'] = encryption
+                networks[essid]['channel'] = channel
+                networks[essid]['mac'] = mac
+                networks[essid]['name'] = essid
+            else:
+                if networks[essid]['signal'] < strength:
+                    networks[essid]['signal'] = strength
+
+        i=i+5
+        myscreen.addstr(i, 1, 'HISTORY:')
+        for x in networks:
+            i=i+1
+            myscreen.addstr(i, 1, networks[x]['signal']+"% ("+networks[x]['encryption']+") "+networks[x]['mac']+" "+networks[x]['name']+" channel "+networks[x]['channel'])
+
+    myscreen.refresh()
+
+while True:
+     main()
+     time.sleep(5)
